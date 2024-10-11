0b13384dc8d125a1445cdcc13608fdccab9a2494
soczo <soczo.levente.711@gmail.com>
2024-10-11 11:11:59 +0200

    Kész

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
+document.getElementById('gomb1').addEventListener('click', function() {
+    const valasz = document.getElementById('valasz1');
+    valasz.classList.toggle('rejtett');
+});
 
+document.getElementById('gomb2').addEventListener('click', function() {
+    const valasz = document.getElementById('valasz2');
+    valasz.classList.toggle('rejtett');
+});
 
+document.getElementById('gomb3').addEventListener('click', function() {
+    const valasz = document.getElementById('valasz3');
+    valasz.classList.toggle('rejtett');
+});
 
+document.getElementById('gomb4').addEventListener('click', function() {
+    const valasz = document.getElementById('valasz4');
+    valasz.classList.toggle('rejtett');
+});
 
+document.getElementById('gomb5').addEventListener('click', function() {
+    const valasz = document.getElementById('valasz5');
+    valasz.classList.toggle('rejtett');
+});
+
+document.getElementById('gomb6').addEventListener('click', function() {
+    const valasz = document.getElementById('valasz6');
+    valasz.classList.toggle('rejtett');
+});
